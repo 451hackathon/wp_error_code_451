2e4a2edf31d2e81e43932347ee5e28ab79edeac4
Ulrike Uhlig <u@451f.org>
2017-07-15 17:36:20 +0200

    This can be done inside the function

--- error_code_451.php
+++ error_code_451.php
@@ -80,12 +80,13 @@ function error_451_check_blocked() {
 	$current_url = $current_url . $_SERVER['REDIRECT_URL'];
 	$post_id = url_to_postid($current_url);
 
+	// get client Geolocation
     $client_geo_origin = get_client_geocode();
 
-    //get blocked countries from post metadata
-    $blocked_countries = explode(',', get_post_meta( $post_id, 'error_451_blocking_countries', true));
-
     if(get_post_meta( $post_id, 'error_451_blocking', true) == "yes" AND isset($client_geo_origin)) {
+		//get blocked countries from post metadata
+		$blocked_countries = explode(',', get_post_meta( $post_id, 'error_451_blocking_countries', true));
+
         if( in_array($client_geo_origin, $blocked_countries) || empty($blocked_countries[0]) ) {
             $error_code = 451;
     		$site_url = site_url();
